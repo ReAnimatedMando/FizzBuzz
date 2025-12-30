97a18746dae297c517948ccfd65f6f3dcf90346c
Aaron Tanguma <the_guma@yahoo.com>
2025-12-30 11:34:47 -0700

    completed whiteboard challenge

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,28 @@
+﻿string FizzBuzz(int number)
+{
+    var buzzWord = "";
+    
+    if (number % 3 == 0)
+    {
+        buzzWord = "fizz";
+    }
+    if (number % 5 == 0)
+    {
+        buzzWord = "buzz";
+    }
+    if (number % 3 == 0 && number % 5 == 0)
+    {
+        buzzWord = "fizzbuzz";
+    }
+    else
+    {
+        buzzWord = "Not Divisible";
+    }
+
+    return buzzWord;
+}
+
+Console.WriteLine(FizzBuzz(9));
+Console.WriteLine(FizzBuzz(10));
+Console.WriteLine(FizzBuzz(15));
+Console.WriteLine(FizzBuzz(7));
