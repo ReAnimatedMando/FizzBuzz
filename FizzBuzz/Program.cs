8bd62d7eb1bd8fbde9449e5f3637ef4518d5d750
Aaron Tanguma <the_guma@yahoo.com>
2025-12-30 19:10:42 -0700

    refactored code using else if statements, also prioritized more specific cases first.

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -2,17 +2,17 @@
 {
     var buzzWord = "";
     
-    if (number % 3 == 0)
-    {
-        buzzWord = "fizz";
-    }
-    if (number % 5 == 0)
-    {
-        buzzWord = "buzz";
-    }
     if (number % 3 == 0 && number % 5 == 0)
     {
         buzzWord = "fizzbuzz";
+    }
+    else if (number % 3 == 0)
+    {
+        buzzWord = "fizz";
+    }
+    else if (number % 5 == 0)
+    {
+        buzzWord = "buzz";
     }
     else
     {
